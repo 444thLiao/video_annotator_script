a41b34f2d1eeec82e575390af56f435f836256f0
Tianhua Liao <l0404th@gmail.com>
2019-04-19 07:30:46 +0800

    partial cal time

--- extra_utils/cal_time.py
+++ extra_utils/cal_time.py
@@ -7,7 +7,6 @@
 import argparse
 import os
 import re
-from ast import literal_eval
 from datetime import timedelta
 from glob import glob
 from itertools import groupby
@@ -16,9 +15,10 @@ from operator import itemgetter
 import cv2
 import numpy as np
 import pandas as pd
-from shapely import wkt
 from tqdm import tqdm
 import os, base64, numpy as np
+from split_video import convert_time_with_rt, parse_rt_f
+
 
 def cal_time(video, dir_path, threshold, rt):
     if os.path.isdir(dir_path):
@@ -56,33 +56,6 @@ def cal_time(video, dir_path, threshold, rt):
         result_df.to_csv(new_name, index=False)
 
 
-def processing_rt(rt, cap):
-    total_frame = cap.get(7)
-    num_frame_per_s = cap.get(5)
-    if rt is None:
-        print('missing starting time')
-    else:
-        frame_time_indic = {}
-        for each in rt:
-            h, m, s, f = map(int, re.split('[:;]', each))
-            frame_time_indic[f] = timedelta(hours=h,
-                                            minutes=m,
-                                            seconds=s)
-        f_2_rt = frame_time_indic.copy()
-        last_one = frame_time_indic[0]
-        last_frame = 0
-        for f in range(total_frame):
-            if f in frame_time_indic and f == 0:
-                f_2_rt[f] = frame_time_indic[0] + timedelta(seconds=f // num_frame_per_s)
-            elif f in frame_time_indic and f != 0:
-                f_2_rt[f] = frame_time_indic[f]
-                last_one = frame_time_indic[f]
-                last_frame = f
-            else:
-                f_2_rt[f] = last_one + timedelta(seconds=(f - last_frame) // num_frame_per_s)
-        return f_2_rt
-
-
 def output_df(time_frame, f_2_rt):
     tmp = []
     for each in time_frame:
@@ -101,42 +74,12 @@ def output_df(time_frame, f_2_rt):
     return new_df
 
 
-def read_points(proj_dir, video, name):
-    default_path = '%s/videos/%s/objects/object*/datasets/%s/path.cvs' % (proj_dir, video, name)
-    if not glob(default_path):
-        print('wrong path %s, Please check the name' % default_path)
-    else:
-        file_path = list(glob(default_path))[0]
-        data = pd.read_csv(file_path, sep=';', index_col=0)
-        return data
-
-
-def read_obj(proj_dir, name):
-    default_path = os.path.join(proj_dir,
-                                'videos',
-                                '*',
-                                'objects',
-                                name,
-                                'data.geo')
-    if not glob(default_path):
-        print('wrong path %s, Please check the name' % default_path)
-        return
-    else:
-        # if name =='R':
-        #     import pdb;pdb.set_trace()
-        file_path = list(glob(default_path))[0]
-        data = literal_eval(open(file_path).read())
-        polygon_data = data[0][1]
-        poly = wkt.loads('POLYGON((%s))' % ', '.join([' '.join([str(v) for v in _]) for _ in (polygon_data + [polygon_data[0]])]))
-        return poly
-        # pt = wkt.loads('POINT(711 287)')
-        #
-        # poly.distance(pt)
-        # poly.boundary.distance(pt)
 from pythonvideoannotator_models.models.video.objects.object2d.object2d_base import Object2dBase
 from pythonvideoannotator_models.models.video.objects.object2d.datasets.contours import Contours
 from pythonvideoannotator_models.models.video import Video
 from pythonvideoannotator_models.models import Project
+
+proj_dir = "D:\\Desktop\\项目\\NORT\\analysis\\20190411"
 
 
 def main_load(project_path):
@@ -144,40 +87,41 @@ def main_load(project_path):
     project.load({}, project_path)
     return project
 
-def get_video(project,name):
-    for v in project.videos:
-        if name in v.name:
-            return v
 
-def load_contours():
-    pass
+def get_geo(video, name):
+    for geo in video.geometries:
+        if geo.name == name:
+            return geo
 
 
-def main(proj_dir, rt, video):
+from shapely import wkt
+from shapely.geometry import Polygon, Point, LinearRing
+from tqdm import tqdm
+
+
+def main(proj_dir):
     project = main_load(proj_dir)
     videos = project.videos
     for video in videos:
         total_frame = video.total_frames
-        for obj in video.objects2D:
-            contour = [_ for _ in obj.datasets if 'contours' in _.name.lower()]
-            if not contour:
-                raise Exception
-            contour = contour[0]
-            extreme_points = map(contour.get_extreme_points,range(total_frame))
-            total_distance = contour.calc_walked_distance(1)[0][-1]
-            
+        L = get_geo(video, 'L')
+        R = get_geo(video, 'R')
+        obj = list(video.objects2D)[0]
+        contour = [_ for _ in obj.datasets if 'contours' in _.name.lower()]
+        contour = contour[0]
+        distance_df = pd.DataFrame(columns=['head to L', 'head to R', 'body to L', 'body to R'])
+        if '_1' in video.name or '_2' in video.name:
+            L_geo = Polygon(L._geometry[0][1])
+            R_geo = Polygon(R._geometry[0][1])
+            extreme_points = [contour.get_extreme_points(_) for _ in tqdm(range(int(total_frame)))]
+            head_list = [_[0] for _ in extreme_points]
+
+            head2L = [L_geo.distance(Point(p)) if p else None for p in head_list]
+            head2R = [R_geo.distance(Point(p)) if p else None for p in head_list]
+
+        total_distance = contour.calc_walked_distance(1)[0][-1]
 
     ############################################################
-    video_name = os.path.basename(video).rpartition('.')[0]
-    p1_data = read_points(proj_dir, video_name, 'p1')
-    p2_data = read_points(proj_dir, video_name, 'p2')
-
-    L_obj = read_obj(proj_dir, 'L')
-    R_obj = read_obj(proj_dir, 'R')
-
-    # video = '/home/liaoth/data2/project/RD_VD/2018111509.mp4'
-    cap = cv2.VideoCapture(video)
-    f_2_rt = processing_rt(rt, cap)
 
     merged_data = p1_data.join(p2_data, rsuffix='_p2')
     tmp = []
@@ -195,13 +139,17 @@ def main(proj_dir, rt, video):
     merged_data.loc[:, ["dis_L_p1", "dis_L_p2", "dis_R_p1", "dis_R_p2"]] = tmp
     return merged_data, f_2_rt, props
 
+
 def match_times(merged_data, f_2_rt, props):
     diff_data = np.apply_along_axis(np.diff, 0, merged_data.values[:, -4:])
-    diff_df = pd.DataFrame(np.append(diff_data, [[0, 0, 0, 0]], 0), index=merged_data.index, columns=merged_data.columns[-4:])
+    diff_df = pd.DataFrame(np.append(diff_data, [[0, 0, 0, 0]], 0), index=merged_data.index,
+                           columns=merged_data.columns[-4:])
     # L obj
-    L_bool_idx = (diff_df.loc[:, ['dis_L_p1', 'dis_L_p2']] <= 1).all(1) & (merged_data.loc[:, ['dis_L_p1', 'dis_L_p2']] <= 45).any(1)
+    L_bool_idx = (diff_df.loc[:, ['dis_L_p1', 'dis_L_p2']] <= 1).all(1) & (
+                merged_data.loc[:, ['dis_L_p1', 'dis_L_p2']] <= 45).any(1)
     # R obj
-    R_bool_idx = (diff_df.loc[:, ['dis_R_p1', 'dis_R_p2']] <= 1).all(1) & (merged_data.loc[:, ['dis_R_p1', 'dis_R_p2']] <= 45).any(1)
+    R_bool_idx = (diff_df.loc[:, ['dis_R_p1', 'dis_R_p2']] <= 1).all(1) & (
+                merged_data.loc[:, ['dis_R_p1', 'dis_R_p2']] <= 45).any(1)
 
     L_time = merged_data.index[L_bool_idx]
     R_time = merged_data.index[R_bool_idx]
@@ -269,7 +217,8 @@ if __name__ == '__main__':
     parser.add_argument("threshold",
                         help="threshold you should give. [default=40]",
                         default=40)
-    parser.add_argument("-rt", nargs='*', help='indicating the time. Using the format like 10:43:15;0 to indicate 10 am 43 min 15 seconds at 0 frame.')
+    parser.add_argument("-rt", nargs='*',
+                        help='indicating the time. Using the format like 10:43:15;0 to indicate 10 am 43 min 15 seconds at 0 frame.')
 
     args = parser.parse_args()
     rt = args.rt
